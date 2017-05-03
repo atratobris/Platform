52b35717223a224210ff6692cd0cba67fc224f26
Paul Cristian <pp13003@my.bristol.ac.uk>
2017-05-03 15:32:46 +0100

    default images on existing boards

--- db/migrate/20170503141859_add_image_url_to_board.rb
+++ db/migrate/20170503141859_add_image_url_to_board.rb
@@ -1,5 +1,8 @@
 class AddImageUrlToBoard < ActiveRecord::Migration[5.1]
   def change
     add_column :boards, :image_url, :string
+    Board.find_each do |b|
+      b.set_image_url
+    end
   end
 end
