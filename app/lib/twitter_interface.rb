11d0ac182cb406507e1d74ba6e9829a8a79f1fa8
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-05-19 11:23:13 +0100

    fix(twitter): Less logs

--- app/lib/twitter_interface.rb
+++ app/lib/twitter_interface.rb
@@ -46,18 +46,10 @@ class TwitterInterface
       end
     end
 
-    # if board.last_tweet.present? && board.last_tweet.dig("url") != last_tweet["url"]
-    #   save_tweet_and_execute last_tweet, board
-    # elsif !board.last_tweet.present? && Time.current - last_tweet["created_at"] < 60.seconds.to_i
-    #   save_tweet_and_execute last_tweet, board
-    # else #Ignore Tweet
-    #   Log.error "TwitterBoard #{board.source} has no new tweet. Last one: #{last_tweet['text']}. Time: #{Time.current}. Last: #{last_tweet['created_at']}. Diff: #{Time.current - last_tweet['created_at']}"
-    #   puts "TwitterInterface: Ignoring Tweet #{last_tweet['handle']} for board #{board.source}"
-    # end
   end
 
   def ignore_tweet last_tweet, board
-    Log.error "TwitterBoard #{board.source} has no new tweet. Last one: #{last_tweet['text']}. Time: #{Time.current}. Last: #{last_tweet['created_at']}. Diff: #{Time.current - last_tweet['created_at']}"
+    # Log.error "TwitterBoard #{board.source} has no new tweet. Last one: #{last_tweet['text']}. Time: #{Time.current}. Last: #{last_tweet['created_at']}. Diff: #{Time.current - last_tweet['created_at']}"
     puts "TwitterInterface: Ignoring Tweet #{last_tweet['handle']} for board #{board.source}"
   end
 
